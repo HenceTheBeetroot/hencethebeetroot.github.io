2cb0888bde153c75891c86427ed512031663edeb
Jesse Tomaskovic <jvt003@bucknell.edu>
2025-05-07 20:51:14 -0400

    Simple cube rendering

--- quest/6/quest6-starter.js
+++ quest/6/quest6-starter.js
@@ -30,94 +30,178 @@ import RayTracer from '/lib/Viz/RayTracer.js'
 import StandardTextObject from '/lib/DSViz/StandardTextObject.js'
 import RayTracingBoxObject from '/lib/DSViz/RayTracingBoxObject.js'
 import Camera from '/lib/Viz/3DCamera.js'
-import MoonSceneRender from '/lib/Moonfall/MoonSceneRenderer.js'
-import { Point, MoonObject } from '/lib/Moonfall/Moon3DObject.js'
-import { getName } from '/lib/Moonfall/General.js'
-import { Collection } from '/lib/Moonfall/Moon3DCollection.js'
-import analyzeSTL, { getModelName } from '/lib/Moonfall/AnalyzeSTL.js'
 
-var objectPaths = [
-  '/assets/STL/ascii/cube.stl',
-  //'/assets/STL/ascii/blender_monkey.stl'
-]
+async function init() {
+  // Create a canvas tag
+  const canvasTag = document.createElement('canvas');
+  canvasTag.id = "renderCanvas";
+  document.body.appendChild(canvasTag);
+  // Create a ray tracer
+  const tracer = new RayTracer(canvasTag);
+  await tracer.init();
+  // Create a 3D Camera
+  var camera = new Camera();
+  // Create an object to trace
+  var tracerObj = new RayTracingBoxObject(tracer._device, tracer._canvasFormat, camera);
+  await tracer.setTracerObject(tracerObj);
+  
+  var fpsText = new StandardTextObject("Loading...");
 
-var collection = new Collection("WORLD")
+  // two types of keys:
+  // "instants" - keys that simply do something when pressed
+  // "perpetuals" - keys that continue affecting the scene until released
+  
+  const keysPressed = new Set();
+  var movespeed = 0.5;
 
-let p = document.createElement('p')
-document.body.appendChild(p)
-for (let path of objectPaths) {
-  console.log("Loading", path)
-  p.innerHTML = "Importing \"" + getModelName(path) + "\" from " + path + " ..."
-  let object = await analyzeSTL(path)
-  console.log("Created object", object)
-  collection.appendData(object)
-  console.log("Loaded!")
+  var helpVisible = true;
+  window.addEventListener('keydown', (e) => {
+    // initiate any perpetuals
+    keysPressed.add(e.key);
+    // handle any instants
+    switch (e.key) {
+      case 'f': case 'F':
+        helpVisible = !helpVisible;
+        break;
+      case 'r': case 'R':
+        camera.resetPose();
+        tracerObj.updateCameraPose();
+        tracerObj.updateCameraFocal();
+        break;
+      case 'p': case 'P':
+        camera._isProjective = !camera._isProjective;
+        tracerObj.updateCameraPose();
+        break;
+      case 'g': case 'G':
+        camera._inverse = !camera._inverse;
+        tracerObj.updateCameraPose();
+    }
+  });
+
+  // this function appropriately disables untoggled perpetuals...
+  window.addEventListener('keyup', (e) => {
+    keysPressed.delete(e.key);
+    // switch (e.key) {
+    //   case 'g': case 'G':
+    //     break;
+    // }
+  });
+
+  // ...and this one handles perpetual functionality
+  const keyboardInputUpdate = (fps) => {
+    for (let key of keysPressed) {
+      switch (key) {
+        // Translation
+        case 'w': case 'W':
+          camera.moveZ(movespeed * 2 / fps);
+          tracerObj.updateCameraPose();
+          break;
+        case 's': case 'S':   
+          camera.moveZ(-movespeed * 2 / fps);
+          tracerObj.updateCameraPose();
+          break;
+        case 'a': case 'A':  
+          camera.moveX(-movespeed * 2 / fps);
+          tracerObj.updateCameraPose();
+          break;
+        case 'd': case 'D': 
+          camera.moveX(movespeed * 2 / fps);
+          tracerObj.updateCameraPose();
+          break;
+        case ' ':  
+          camera.moveY((camera._inverse? 1 : -1) * movespeed * 2 / fps);
+          tracerObj.updateCameraPose();
+          break;
+        case 'Shift': 
+          camera.moveY((camera._inverse? -1 : 1) * movespeed * 2 / fps);
+          tracerObj.updateCameraPose();
+          break;
+        
+        // Rotation
+        case 'i': case 'I':
+          camera.rotateX(-movespeed * 2 / fps);
+          tracerObj.updateCameraPose();
+          break;
+        case 'k': case 'K':   
+          camera.rotateX(movespeed * 2 / fps);
+          tracerObj.updateCameraPose();
+          break;
+        case 'j': case 'J':
+          camera.rotateY((camera._inverse? -1 : 1) * movespeed * 2 / fps);
+          tracerObj.updateCameraPose();
+          break;
+        case 'l': case 'L':   
+          camera.rotateY((camera._inverse? 1 : -1) * movespeed * 2 / fps);
+          tracerObj.updateCameraPose();
+          break;
+        case 'u': case 'U':
+          camera.rotateZ((camera._inverse? -1 : 1) * movespeed * 2 / fps);
+          tracerObj.updateCameraPose();
+          break;
+        case 'o': case 'O':   
+          camera.rotateZ((camera._inverse? 1 : -1) * movespeed * 2 / fps);
+          tracerObj.updateCameraPose();
+          break;
+
+        // Focal Length
+        case 'q': case 'Q':
+          camera.scaleFocal(1 - (movespeed * 2 / fps * Math.sign(tgtFPS)));
+          tracerObj.updateCameraFocal();
+          break;
+        case 'e': case 'E':
+          camera.scaleFocal(1 + (movespeed * 2 / fps * Math.sign(tgtFPS)));
+          tracerObj.updateCameraFocal();
+          break;
+      }
+    };
+  }
+  
+  // run animation at 60 fps
+  var frameCnt = 0;
+  var tgtFPS = 60;
+  var secPerFrame = 1. / tgtFPS;
+  var frameInterval = secPerFrame * 1000;
+  var lastCalled;
+  let renderFrame = () => {
+    keyboardInputUpdate(tgtFPS);
+    let elapsed = Date.now() - lastCalled;
+    if (elapsed > frameInterval) {
+      ++frameCnt;
+      lastCalled = Date.now() - (elapsed % frameInterval);
+      tracer.render();
+    }
+    requestAnimationFrame(renderFrame);
+  };
+  lastCalled = Date.now();
+  renderFrame();
+  setInterval(() => { 
+    let text = `fps: ${frameCnt}` +
+    `\n[ F ] ${helpVisible? "Hide" : "Show"} controls` +
+    (helpVisible?
+    '\n[ R ] Reset view' +
+    `\n[ P ] Change view type (currently ${camera._isProjective? "projective" : "orthographic"})` +
+    `\n[ G ] Move camera / object (currently ${camera._inverse? "object" : "camera"})` +
+    `\n      (Uses light orange face as "front" and gray as "top")` +
+    '\n\n[ W/S ] Dolly forward/backward' +
+    '\n[ A/D ] Truck left/right' +
+    '\n[ Space/Shift ] Boom up/down' +
+    '\n\n[ I/K ] Tilt up/down' +
+    '\n[ J/L ] Pan left/right' +
+    '\n[ U/O ] Roll clockwise/counterclockwise' +
+    '\n\n[ E/Q ] FOV in/out'
+    : "")
+    fpsText.updateTextRegion(text);
+    fpsText.updateText(text);
+    frameCnt = 0;
+  }, 1000); // call every 1000 ms
+  return tracer;
 }
-console.log(collection)
-document.body.removeChild(p)
 
-let renderer = new MoonSceneRender()
-renderer.addToScene(collection)
-
-renderer.init()
-
-// async function init() {
-//   if (!navigator.gpu) {
-//     throw Error("WebGPU is not supported in this browser.")
-//   }
-
-//   const adapter = await navigator.gpu.requestAdapter()
-//   if (!adapter) {
-//     throw Error("Failed to request adapter.")
-//   }
-
-//   const device = await adapter.requestDevice()
-
-//   // Create a canvas tag
-//   const canvasTag = document.createElement('canvas');
-//   canvasTag.id = "renderCanvas";
-//   document.body.appendChild(canvasTag);
-//   // Create a ray tracer
-//   const tracer = new RayTracer(canvasTag);
-//   await tracer.init();
-//   // Create a 3D Camera
-//   var camera = new Camera();
-//   // Create an object to trace
-//   var tracerObj = new RayTracingBoxObject(tracer._device, tracer._canvasFormat, camera);
-//   await tracer.setTracerObject(tracerObj);
-  
-//   let fps = '??';
-//   var fpsText = new StandardTextObject('fps: ' + fps);
-  
-//   // run animation at 60 fps
-//   var frameCnt = 0;
-//   var tgtFPS = 60;
-//   var secPerFrame = 1. / tgtFPS;
-//   var frameInterval = secPerFrame * 1000;
-//   var lastCalled;
-//   let renderFrame = () => {
-//     let elapsed = Date.now() - lastCalled;
-//     if (elapsed > frameInterval) {
-//       ++frameCnt;
-//       lastCalled = Date.now() - (elapsed % frameInterval);
-//       tracer.render();
-//     }
-//     requestAnimationFrame(renderFrame);
-//   };
-//   lastCalled = Date.now();
-//   renderFrame();
-//   setInterval(() => { 
-//     fpsText.updateText('fps: ' + frameCnt);
-//     frameCnt = 0;
-//   }, 1000); // call every 1000 ms
-//   return tracer;
-// }
-
-// init().then( ret => {
-//   console.log(ret);
-// }).catch( error => {
-//   const pTag = document.createElement('p');
-//   pTag.innerHTML = navigator.userAgent + "</br>" + error.message;
-//   document.body.appendChild(pTag);
-//   document.getElementById("renderCanvas").remove();
-// });
+init().then( ret => {
+  console.log(ret);
+}).catch( error => {
+  const pTag = document.createElement('p');
+  pTag.innerHTML = navigator.userAgent + "</br>" + error.message;
+  document.body.appendChild(pTag);
+  document.getElementById("renderCanvas").remove();
+});
